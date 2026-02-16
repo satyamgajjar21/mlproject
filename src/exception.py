76d48aa44213669e6718da2f35aff10b0da87125
Satyam Gajjar <66769510+satyamgajjar21@users.noreply.github.com>
2026-02-16 17:40:05 +0530

    Logging exception

---
+++ src/exception.py
@@ -0,0 +1,33 @@
+import sys
+
+def error_message_detail(error, error_detail: sys):
+    """
+    This function returns detailed error message including file name and line number.
+    """
+    _, _, exc_tb = error_detail.exc_info()
+
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = f"Error occurred in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+#What This Does
+
+# Instead of basic error like:
+# ValueError: invalid literal
+
+# You will get:
+# Error occurred in python script name [data_ingestion.py] 
+# line number [45] 
+# error message [File not found]
