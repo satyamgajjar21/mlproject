a7410f39dc06e20fa3ff28e944fe76e0d26681b6
Satyam Gajjar <66769510+satyamgajjar21@users.noreply.github.com>
2026-02-20 17:45:00 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -20,8 +21,57 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
     
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            
+            model.fit(X_train, y_train) #train the model
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]] = test_model_score    
+            
+            return report
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 #     ✔ Creates folder if needed
 # ✔ Saves Python object as binary
 # ✔ Uses dill for serialization
 # ✔ Used to save models & preprocessors
 # ✔ Keeps project clean and modular
+
+
+
+
+#utils file contains
+# save_object()
+
+# load_object()
+
+# evaluate_model()
